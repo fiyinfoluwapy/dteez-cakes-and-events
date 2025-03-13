b7edb6fb73b29d158debb4b380207273eedc42fd
Temidire Damian <vladimirwayne9@gmail.com>
2025-03-13 22:17:40 +0000

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "bg-red-500", "text-white", "font-bold", "flex", "items-center", "justify-center",
+    "min-h-screen", "text-4xl", "bg-gray-900", "text-white"
+  ],
   theme: {
     extend: {},
   },
