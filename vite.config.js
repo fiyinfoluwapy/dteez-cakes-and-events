275b81e535d545da69a84dcae5e1683fb308c25a
Temidire Damian <vladimirwayne9@gmail.com>
2025-03-14 07:05:46 +0000

    Fixed Tailwind PostCSS issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js', // Ensure Vite uses PostCSS correctly
+  },
 });
